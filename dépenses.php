891663cfb4bbf9b67ba32a10175b0456a2f3921a
KiriMashi32 <bastien.lavaux@gmail.com>
2023-02-23 10:37:04 +0100

    new popup trop beau gosse wallali

--- dépenses.php
+++ dépenses.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if(isset($_POST['submit'])){
+    $_SESSION['popup'] = true;
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,147 +13,76 @@
     <link rel="icon" type="image/jpg" href="logo.jpg" />
     <title>Ma banque</title>
     <style>
-        h1, h2{
-        text-align: center;
+        h1, h2 {
+            text-align: center;
         }
-        .confirm{
+
+        .confirm {
             color: green;
         }
 
-        .erreur{
+        .erreur {
             color: red;
         }
 
-        .nav_bar{
+        .nav_bar {
             display: flex;
         }
 
-        .confirm{
-            color: green;
+        /* Ajouter une position relative au conteneur */
+        .container {
+            position: relative;
         }
-        
-        /* Popup container - can be anything you want */
+
+        /* Modifier la position du popup */
         .popup {
-        position: relative;
-        display: inline-block;
-        cursor: pointer;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
+            display: block;
+            position: absolute;
+            bottom: 100%; /* Afficher le popup juste en dessous du bouton */
+            right: 0;
+            background-color: #f1f1f1;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0px 0px 10px 2px rgba(0,0,0,0.3);
+            opacity: 0;
+            transition: opacity 0.5s ease-in-out;
         }
 
-        /* The actual popup */
-        .popup .popuptext {
-        visibility: hidden;
-        width: 160px;
-        background-color: #555;
-        color: #fff;
-        text-align: center;
-        border-radius: 6px;
-        padding: 8px 0;
-        position: absolute;
-        z-index: 1;
-        bottom: 125%;
-        transform : translateY(200%);
-        left: 50%;
-        margin-left: -80px;
+        .popup.show {
+            opacity: 1;
         }
+    </style>
 
-
-        /* Toggle this class - hide and show the popup */
-        .popup .show {
-        visibility: visible;
-        -webkit-animation: fadeIn 1s;
-        animation: fadeIn 1s;
+    <script>
+        window.onload = function() {
+            <?php
+            if(isset($_SESSION['popup'])){
+                unset($_SESSION['popup']);
+            ?>
+            var popup = document.getElementById("popup");
+            popup.classList.add("show");
+            setTimeout(function(){
+                popup.classList.remove("show");
+            }, 5000);
+            <?php } ?>
         }
-
-        /* Add animation (fade in the popup) */
-        @-webkit-keyframes fadeIn {
-        from {opacity: 0;} 
-        to {opacity: 1;}
-        }
-
-        @keyframes fadeIn {
-        from {opacity: 0;}
-        to {opacity:1 ;}
-        }
-    </style>   
-        
-        /* Popup container - can be anything you want */
-        .popup {
-        position: relative;
-        display: inline-block;
-        cursor: pointer;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-        }
-
-        /* The actual popup */
-        .popup .popuptext {
-        visibility: hidden;
-        width: 160px;
-        background-color: #555;
-        color: #fff;
-        text-align: center;
-        border-radius: 6px;
-        padding: 8px 0;
-        position: absolute;
-        z-index: 1;
-        bottom: 125%;
-        transform : translateY(200%);
-        left: 50%;
-        margin-left: -80px;
-        }
-
-
-        /* Toggle this class - hide and show the popup */
-        .popup .show {
-        visibility: visible;
-        -webkit-animation: fadeIn 1s;
-        animation: fadeIn 1s;
-        }
-
-        /* Add animation (fade in the popup) */
-        @-webkit-keyframes fadeIn {
-        from {opacity: 0;} 
-        to {opacity: 1;}
-        }
-
-        @keyframes fadeIn {
-        from {opacity: 0;}
-        to {opacity:1 ;}
-        }
-    </style>  
-
-    
+    </script>
 </head>
-<body>
-    
-</body>
 <body style="text-align:center">
     <h1><b>Mes dépenses</b></h1>
     <h2>FR13 1273 9000 7064 3341 7217 M62</h2>
     <h3>Effectuer un virement</h3>
-    <form action="dépenses.php" method="post">
-        <input type="text" id="virement" name="destinataire" placeholder="RIB du destinataire"><br><br>
-        <input type="text" id="virement" name="virement" placeholder="Somme"><br><br>
-        <!-- <input type="submit" name="send" class="button" value="Envoyer" /> -->
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <div class="container">
+            <input type="text" id="destinataire" name="destinataire" placeholder="RIB du destinataire"><br><br>
+            <input type="text" id="virement" name="virement" placeholder="Somme"><br><br>
+            <button type="submit" name="submit">Cliquez ici</button>
+            <!-- Ajouter le popup dans le même conteneur que le bouton -->
+            <div id="popup" class="popup">
+                <p>Texte à afficher dans le popup</p>
+            </div>
+        </div>
     </form>
-    <div class="popup" onclick="myFunction()">Effectuer le virement 
-        <span class="popuptext" id="myPopup">Virement effectué ! </span>
-    </div>
-    <script>
-        // When the user clicks on div, open the popup
-        function myFunction() {
-            var popup = document.getElementById("myPopup");
-            popup.classList.toggle("show");
-        }
-</script>
-   
 
 </body>
 </html>
-    
