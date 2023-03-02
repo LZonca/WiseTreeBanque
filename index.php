b8abe71a17445d3a7832afc4a546289451eebfd1
Léo <leo.zonca@business-school.academy>
2023-03-02 09:32:01 +0100

    reparaiton

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start();
 function loginrequest()
 {
     try{
-    $bdd = new PDO('mysql:host=;dbname=wisebankdb;charset=utf8', 'phpmyadmin', 'carriat');
+    //$bdd = new PDO('mysql:host=;dbname=wisebankdb;charset=utf8', 'phpmyadmin', 'carriat');
     $bdd = new PDO('mysql:host=localhost;dbname=wisebankdb;charset=utf8', 'root','');
 
     }catch(exception $e){
