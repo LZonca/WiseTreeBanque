1245aa30b1854ea68bfdc619b9ffd09c8abc02a9
LZonca <leo.zonca@business-school.academy>
2023-05-02 21:03:06 +0200

    session destroy pour etre sur pas de session open

--- logout.php
+++ logout.php
@@ -3,6 +3,7 @@ session_start();
     unset($_SESSION['userid']);
     echo "<h1>Deconnexion en cours !</h1>";
     unset($_SESSION['usermessage']);
+    session_destroy();
     header('Location: Connexion');
 
     if(!isset($_SESSION))
